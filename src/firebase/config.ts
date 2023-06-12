71794a2ee62b1867233a641b97be2cdc32c88968
Camilo Uzquiano <camilo.uzquiano@outlook.com>
2023-06-11 23:12:40 -0400

    Add env file

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -7,13 +7,13 @@ import { connectFunctionsEmulator, getFunctions } from "firebase/functions";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDLXp0t1of8_GjfBj8Z1UmeoMzQclF5eT4",
-  authDomain: "excel-weather.firebaseapp.com",
-  projectId: "excel-weather",
-  storageBucket: "excel-weather.appspot.com",
-  messagingSenderId: "286735541394",
-  appId: "1:286735541394:web:d507773d1402868f41724b",
-  measurementId: "G-CV5P6MB65J",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
